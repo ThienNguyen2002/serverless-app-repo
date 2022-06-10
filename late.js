977cc0bde009ab3f2a49fec33c02b7ee8bbbc46f
THIEN NGUYEN <u1266885@umail.utah.edu>
2022-06-09 18:55:09 -0600

    Updated time

--- late.js
+++ late.js
@@ -1,15 +1,15 @@
 // start coding your function here!
-//a function that returns "It is late!" 
+//a function that returns "It is late!"
 //if that time is at or after 10:00 pm,
-//and "It is still early!" if it is not. 
+//and "It is still early!" if it is not.
 function running_late(date) {
-    
-    var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-  if (time >= "22:00:00") {
+  date.toLocaleTimeString("it-IT");
+  if (date >= "22:00:00") {
     return "It is late!";
   } else {
     return "It is still early!";
   }
 }
 
+console.log(running_late("December 17, 1995 21:24:00"));
 exports.running_late = running_late;
