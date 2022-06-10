c7d256db72dfba5247291d8c8a6a038a5185cb43
THIEN NGUYEN <u1266885@umail.utah.edu>
2022-06-09 20:16:01 -0600

    ....

--- late.js
+++ late.js
@@ -1,8 +1,7 @@
 // start coding your function here!
 // Create a function running_late(date) in JavaScript that returns It is late! if the time is at or after 10:00 PM, and It is still early! if it is not.
 function running_late(date) {
-  var theDate = new Date(date);
-  var time = theDate.getHours();
+  var time = new Date(date).getHours();
   if (time >= 22) {
     return "It is late!";
   } else {
