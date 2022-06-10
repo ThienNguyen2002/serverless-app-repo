22908cb18da10ae4a4246a57df58e85cfe41ef03
THIEN NGUYEN <u1266885@umail.utah.edu>
2022-06-09 18:30:55 -0600

    Added running_late function

--- late.js
+++ late.js
@@ -1,1 +1,13 @@
 // start coding your function here!
+//a function that returns "It is late!" 
+//if that time is at or after 10:00 pm,
+//and "It is still early!" if it is not. 
+function running_late(date) {
+  if (date >= "22:00") {
+    return "It is late!";
+  } else {
+    return "It is still early!";
+  }
+}
+
+exports.running_late = running_late;
