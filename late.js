7b654826ea5712192a3cfd1a548da7f011479d06
THIEN NGUYEN <u1266885@umail.utah.edu>
2022-06-09 18:44:26 -0600

    Formated time to hours, minutes and seconds

--- late.js
+++ late.js
@@ -3,7 +3,8 @@
 //if that time is at or after 10:00 pm,
 //and "It is still early!" if it is not. 
 function running_late(date) {
-    var time = date.getHours();
+    
+    var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
   if (time >= "22:00:00") {
     return "It is late!";
   } else {
