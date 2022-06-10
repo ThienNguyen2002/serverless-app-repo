015fe766d5431c8a6249e37c3105871e32c20fb3
THIEN NGUYEN <u1266885@umail.utah.edu>
2022-06-09 20:01:50 -0600

    Added running_late function

--- late.js
+++ late.js
@@ -1,1 +1,7 @@
 // start coding your function here!
+function running_late(date) {
+  var hours = date.getHours();
+  if (hours >= 22) return "It is late!";
+  else return "It is still early!";
+}
+exports.running_late = running_late;
